a5462733fd3d03c62350beba4d259f79226827c9
mgDentist <gmi1987@gmail.com>
2023-12-07 14:17:12 +0400

    добавляет bff registration

---
+++ src/bff/bff.js
@@ -0,0 +1,87 @@
+const generateDate = () => new Date(Math.random() * 1000000000000 + 1999999999999)
+    .toISOString()
+    .substring(0, 16)
+    .replace('T', ' ');
+
+export const server = {
+    async autorize(authLogin, authPassword) {
+        const users = await fetch('http://localhost:3005/users')
+            .then((loadedUsers) => loadedUsers.json()
+            );
+
+        const user = users.find(({ login }) => login === authLogin);
+
+        if (!user) {
+            return {
+                error: 'Такой пользователь не найден',
+                res: null,
+            };
+        }
+
+        if (authPassword !== user.password) {
+            return {
+                error: 'Неверный пароль',
+                res: null,
+            };
+        }
+        
+        const session = {
+            logout() {
+                Object.keys(session).forEach((key) => {
+                    delete session[key]
+                })
+            },
+            removeComment() {
+                console.log('Удаление комментария');
+            },
+        }
+
+        return {
+            error: null,
+            res: session,
+        }
+    },
+    async register(regLogin, regPassword) {
+        const users = await fetch('http://localhost:3005/users')
+            .then((loadedUsers) => loadedUsers.json()
+            );
+
+        const user = users.find(({ login }) => login === regLogin);
+
+        if (user) {
+            return {
+                error: 'Такой логин уже занят',
+                res: null,
+            };
+        }
+        
+        await fetch('http://localhost:3005/users', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json;charset=utf-8'
+            },
+            body: JSON.stringify({
+                login: regLogin,
+                password: regPassword,
+                registed_at: ,
+                role_id: 2,
+            }),
+        });
+
+        const session = {
+            logout() {
+                Object.keys(session).forEach((key) => {
+                    delete session[key]
+                })
+            },
+            removeComment() {
+                console.log('Удаление комментария');
+            },
+        };
+        
+        return {
+            error: null,
+            res: session,
+        };
+    },
+};
